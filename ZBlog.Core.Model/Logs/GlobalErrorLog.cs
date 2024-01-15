838659c292367f4c4fc3485f5f4c5c1ff78717fc
LL zhang <zhang31666@outlook.com>
2024-01-15 23:22:01 +0800

    日志模型

---
+++ ZBlog.Core.Model/Logs/GlobalErrorLog.cs
@@ -0,0 +1,14 @@
+﻿using SqlSugar;
+using ZBlog.Core.Model.Base;
+
+namespace ZBlog.Core.Model.Logs
+{
+    [Tenant("log")]
+    [SplitTable(SplitType.Month)]   //按月分表 （自带分表支持 年、季、月、周、日）
+    [SugarTable($@"{nameof(GlobalErrorLog)}_{{year}}{{month}}{{day}}")]
+    public class GlobalErrorLog : BaseLog
+    {
+        [SugarColumn(IsNullable = true, ColumnDataType = "longtext,text,clob")]
+        public string Exception { get; set; }
+    }
+}
