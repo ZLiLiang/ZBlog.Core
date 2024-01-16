2a5b65a74ba0fac1254b6ae51b0aba356be156ce
LL zhang <zhang31666@outlook.com>
2024-01-16 21:40:29 +0800

    把属性开头改为大写

--- ZBlog.Core.Model/Models/TestModels.cs
+++ ZBlog.Core.Model/Models/TestModels.cs
@@ -2,11 +2,11 @@
 {
     public class TestMuchTableResult
     {
-        public string moduleName { get; set; }
-        public string permName { get; set; }
-        public long rid { get; set; }
-        public long mid { get; set; }
-        public long? pid { get; set; }
+        public string ModuleName { get; set; }
+        public string PermName { get; set; }
+        public long Rid { get; set; }
+        public long Mid { get; set; }
+        public long? Pid { get; set; }
 
     }
 }
